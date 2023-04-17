5ae52c77aea93202bf276a3bb2871ad34f64053b
Afzal <130027973+af-zal@users.noreply.github.com>
2023-04-17 20:03:55 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
